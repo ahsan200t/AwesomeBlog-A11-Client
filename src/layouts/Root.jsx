f05c6be50ec945ba33fbcf7b59c67e9fb32a39b1
ahsan200t <ahsanpekua200@gmail.com>
2024-05-09 23:05:12 +0600

    Project Setup Complete

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,13 @@
+const Root = () => {
+    return (
+        <div>
+            {/* Navbar */}
+             <h1>Main</h1>
+            {/* Outlet */}
+
+            {/* Footer */}
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+// Social Auth Provider
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+const auth = getAuth(app);
+
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserProfile = (fullName, photoURL, email) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: fullName,
+      photoURL: photoURL,
+      email: email,
+    });
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setUser(user);
+      }else{
+        setUser(false)
+      }
+    });
+    return () => unSubscribe();
+  }, [user]);
+
+  //   Google Login
+
+  const googleLogIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+   //   Github Login
+
+   const githubLogIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  const logOut = () => {
+    setUser(false)
+    return signOut(auth);
+   
+  };
+  const userInfo = {
+    googleLogIn,
+    githubLogIn,
+    user,
+    setUser,
+    loading,
+    createUser,
+    signInUser,
+    logOut,
+    updateUserProfile,
+  };
+  return (
+    <AuthContext.Provider value={userInfo}>
+        {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
